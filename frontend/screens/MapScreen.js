08c356a330415c8edf1f8baf9ba98fa5afe4511e
Tess Bergman <aeridona@gmail.com>
2022-09-28 19:28:44 -0400

    Placed Map Framework, NYC origin, Marker

--- frontend/screens/MapScreen.js
+++ frontend/screens/MapScreen.js
@@ -1,5 +1,15 @@
 import * as React from "react";
-import { View,Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import { Polyline } from "react-native-maps";
+
+const nycRegion = {
+    latitude: 40.730610,
+    longitude: -73.935242,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
 
 export default function MapScreen({ navigation }) {
     return (
@@ -11,6 +21,28 @@ export default function MapScreen({ navigation }) {
                 
                 Map Screen
             </Text>
+            <MapView
+
+            style={styles.map}
+
+            initialRegion={nycRegion}
+            >
+             <Marker coordinate={nycRegion} />
+
+            </MapView>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, 
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
