a15e073e6442b902c4b29c29e1d3028a2b841ef4
hasiddiqui2 <Hammad.Siddiqui32@myhunter.cuny.edu>
2022-09-29 01:11:57 -0400

    initialized itinerary screen, created search bar placeholder, added proper padding

--- frontend/screens/ItineraryScreen.js
+++ frontend/screens/ItineraryScreen.js
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { View,Text } from "react-native";
 
+
 export default function ItineraryScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', paddingTop: 50}}>
             <Text
                 onPress={() => alert('Placeholder Itinerary screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>
+                style={{ fontSize: 26, fontWeight: 'bold'}}>
                     
                 
-                Itinerary Screen Testing
+                Itching for a New Trip?{'\n'}
+                We can help with that.{'\n'}
+                First off, let's see where{'\n'}
+                you're starting at.
             </Text>
+            <Text
+                onPress={() => alert('Placeholder Search bar')}
+                style={{ fontSize: 26, fontWeight: 'bold', paddingTop: 50}}>
+                
+                Search Bar Placeholder
+            </Text>
+            
         </View>
     );
 }
