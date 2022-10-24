639f9eff1e0fce2f2ab30f21e51ac7071bc5b70f
MorisGoldshtein <morisgoldshtein@Moriss-MacBook-Pro.local>
2022-10-24 16:52:08 -0400

    headers removed for screens

--- frontend/MainContainer.js
+++ frontend/MainContainer.js
@@ -19,7 +19,6 @@ const Tab = createBottomTabNavigator();
 
 function MainContainer() {
   return (
-    <NavigationContainer>
       <Tab.Navigator
         initialRouteName={mapName}
         screenOptions={
@@ -48,6 +47,7 @@ function MainContainer() {
                 tabBarInactiveTintColor: 'grey',
                 tabBarLabelStyle: { paddingBottom: 4, fontSize: 12 },
                 tabBarStyle: { paddingBottom: 15, height: 65, paddingTop: 8},
+                headerShown:false
             })
         }
         >
@@ -58,7 +58,6 @@ function MainContainer() {
         <Tab.Screen name={profileName} component={ProfileScreen} />
 
       </Tab.Navigator>
-    </NavigationContainer>
   );
 }
 
